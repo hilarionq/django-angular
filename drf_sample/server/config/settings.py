94ab770994275086fb86c378d53256dfd2dd8079
hilarionq <hilarionq@gmail.com>
2017-08-16 11:17:35 -0400

    Fix  'No 'Access-Control-Allow-Origin ':  Installed django-cors-headers app in django server application. https://github.com/ottoyiu/django-cors-headers

--- drf_sample/server/config/settings.py
+++ drf_sample/server/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'retail',
 ]
 
@@ -48,8 +49,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
-
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8081',
+    '127.0.0.1:8081'
+)
 ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
